77a375e5640dfb16afbebda71d44448acb3c4300
Connor <connorjameslow@gmail.com>
2021-06-22 08:54:05 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+
+Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+
+            webBuilder.Configure(app =>
+            {
+                app.Use(async (context, _) =>
+                {
+                    await using FileStream stream = File.OpenRead("index.html");
+                    context.Response.ContentType = "text/html";
+                    await stream.CopyToAsync(context.Response.Body);
+                });
+            });
+        })
+        .Build()
+        .Run();
