f0c90f40f132921e23a26dd0c68e9f4beca6f973
Connor <connorjameslow@gmail.com>
2021-06-22 12:13:54 -0700

    Simplify Program.cs

--- Program.cs
+++ Program.cs
@@ -6,18 +6,11 @@ using System;
 using System.IO;
 
 Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder =>
+        .ConfigureWebHostDefaults(webBuilder => webBuilder.Configure(app => app.Use(async (context, _) =>
         {
-
-            webBuilder.Configure(app =>
-            {
-                app.Use(async (context, _) =>
-                {
-                    await using FileStream stream = File.OpenRead("index.html");
-                    context.Response.ContentType = "text/html";
-                    await stream.CopyToAsync(context.Response.Body);
-                });
-            });
-        })
+            await using FileStream stream = File.OpenRead("index.html");
+            context.Response.ContentType = "text/html";
+            await stream.CopyToAsync(context.Response.Body);
+        })))
         .Build()
         .Run();
